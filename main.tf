822c6099599ccbfb59eea17e771e984bf381a7c4
shanewxy <592491808@qq.com>
2023-11-16 09:53:23 +0800

    refactor: modify ttl

--- main.tf
+++ main.tf
@@ -197,7 +197,7 @@ resource "aws_service_discovery_service" "primary" {
     namespace_id   = data.aws_service_discovery_dns_namespace.selected.id
     routing_policy = "WEIGHTED"
     dns_records {
-      ttl  = 10
+      ttl  = 30
       type = "CNAME"
     }
   }
@@ -222,7 +222,7 @@ resource "aws_service_discovery_service" "reader" {
     namespace_id   = data.aws_service_discovery_dns_namespace.selected.id
     routing_policy = "WEIGHTED"
     dns_records {
-      ttl  = 10
+      ttl  = 30
       type = "CNAME"
     }
   }
